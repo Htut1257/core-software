4222dc4d74fa46e2131d122796ac51f5b6e94b8d
Htut1257 <myohtut1257@gmail.com>
2022-07-14 10:15:42 +0630

    latest update

---
+++ src/app/api/app-api-setting.ts
@@ -0,0 +1,19 @@
+export class ApiSetting{
+    public static get UserApiEndPoint(){
+        return 'http://192.168.100.233:8079';
+    }
+
+    public static get InventoryApiEndPoint(){
+        return 'http://192.168.100.233:8078';
+    }
+
+    public static get RedisDataEndPoint(){
+        return 'redis://default:redis-14451.c16.us-east-1-3.ec2.cloud.redislabs.com:14451@redis-14451.c16.us-east-1-3.ec2.cloud.redislabs.com:14451'
+    }
+
+    public static get payRollApi(){
+        return 
+    }
+
+
+}
